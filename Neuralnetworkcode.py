5c41526d447a2758232c47d77d792843b040e7a4
Anant Krisn Bais <1anantkbais@gmail.com>
2022-02-07 15:58:40 +0530

    update

--- Neuralnetworkcode.py
+++ Neuralnetworkcode.py
@@ -1,7 +1,7 @@
 '''Trains a simple convnet on the MNIST dataset.
 Gets to 99.21% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
-128 seconds per epoch on a intel i5 CPU.
+
 '''
 
 from __future__ import print_function
